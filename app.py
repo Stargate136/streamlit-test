0aae3704e99426e844237203c716435226a0f2a8
Christian Aucane <christianaucane1@gmail.com>
2023-09-06 11:11:25 +0200

    Modify app

--- app.py
+++ app.py
@@ -1,40 +1,61 @@
 import streamlit as st
-
 import pandas as pd
-
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-st.title('Hello Wilders, welcome to my application!')
+def show_graphs(df):
+    st.subheader("Heatmap de corrélation")
+    ax_heatmap = sns.heatmap(df.select_dtypes(include="number").corr(), annot=True)
+    st.pyplot(ax_heatmap.get_figure())
+    st.subheader("Histogrammes de distribution")
+    
+    n = len(df.columns)
+    fig_dist, axes = plt.subplots(n, 1, figsize=(5, 4*n))
 
+    for idx, col in enumerate(df.columns):
+        df[col].hist(ax=axes[idx])
+        axes[idx].set_title(col)
 
-st.write("I enjoy to discover stremalit possibilities")
+    st.pyplot(fig_dist)
+        
 
+def main():
+    st.title('Bonjour, bienvenu sur ma première application Streamlit')
+    st.write("Je suis ravi de découvrir les posibilités de streamlit!")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+    link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 
-df_weather = pd.read_csv(link)
+    df = pd.read_csv(link)
+    show_filtered = False
+	
+    filter = "Pas de filtre"
 
-df_weather
+    st.header("Filtres")
+    btn_cols = st.columns(4)
+    if btn_cols[0].button("Pas de filtre"):
+        show_filtered = False
+        filter = "Pas de filtre"
+    if btn_cols[1].button("US") :
+        df_filtered = df[df["continent"] == " US."]
+        show_filtered = True
+        filter = "US"
+    if btn_cols[2].button("Europe") :
+        df_filtered = df[df["continent"] == " Europe."]
+        show_filtered = True
+        filter = "Europe"
+    if btn_cols[3].button("Japan") :
+        df_filtered = df[df["continent"] == " Japan."]
+        show_filtered = True
+        filter = "Japan"
+        
+    st.header(f"Analyse avec le filtre '{filter}'")
+    
+    if show_filtered:
+        show_graphs(df_filtered)
+    else:
+         show_graphs(df)
+         
 
-df_weather["DATE"] = pd.to_datetime(df_weather["DATE"])
-
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-
-viz_correlation = sns.heatmap(df_weather.select_dtypes(include="number").corr(), 
-
-								center=0,
-
-								cmap = sns.color_palette("vlag", as_cmap=True)
-
-								)
-
-
-st.pyplot(viz_correlation.figure)
-
-
-name = st.text_input("Please give me your name :")
-
-name_length = len(name)
-
-st.write("Your name has ",name_length,"characters")
+if __name__ == "__main__":
+	main()
